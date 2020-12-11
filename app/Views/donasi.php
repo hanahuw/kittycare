7fbf90532b72582ac355b4123c72eb987d260968
AdeliaMln <adelia.melanie261@gmail.com>
2020-12-11 10:16:26 +0700

    2

--- app/Views/donasi.php
+++ app/Views/donasi.php
@@ -1,5 +1,63 @@
 <?= $this->extend('layout/template'); ?>
 
 <?= $this->section('content'); ?>
-<h2> Hallo ini halaman donasi </h2>
+<h2> Ayo Donasi </h2>
+<form class="row g-3">
+  <div class="col-12">
+    <label for="inputnama" class="form-label">Nama Lengkap</label>
+    <input type="text" class="form-control" id="inputnama">
+  </div>
+  <div class="col-12">
+    <label for="inputemail" class="form-label">Email</label>
+    <input type="email" class="form-control" id="inputemail">
+  </div>
+  
+  <div class="col-12">
+    <label for="inputpaket" class="form-label">Paket</label>
+    <select id="inputpaket" class="form-select">
+      <option selected>Silahkan Pilih</option>
+      <option value="" selected="selected">
+        Silakan Pilih
+        </option>
+                                        
+        <option value="pengobatan">
+        Pengobatan
+        </option>
+                                  
+        <option value="Obat Cacing  Tetes Kutu">
+        Obat Cacing &amp; Tetes Kutu
+        </option>
+                                                            
+        <option value="Sterilisasi Jantan ">
+        Sterilisasi Jantan
+        </option>  
+                                                            
+        <option value="Sterilisasi Betina ">
+        Sterilisasi Betina 
+        </option>
+                                      
+        <option value="TNR Kecil 5 kucing">
+        TNR Kecil (5 kucing)
+        </option>
+                                        
+        <option value="TNR Besar 10 kucing">
+        TNR Besar (10 kucing)
+        </option>
+    </select>
+  </div>
+  <div class="col-12">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <label class="form-check-label" for="gridCheck">
+        Setuju
+      </label>
+    </div>
+  </div>
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary">Donasi</button>
+  </div>
+</form>
+
+  <br><br>
+
 <?= $this->endSection('content'); ?>
--- app/Views/hubungikami.php
+++ app/Views/hubungikami.php
@@ -1,5 +1,25 @@
 <?= $this->extend('layout/template'); ?>
 
 <?= $this->section('content'); ?>
-<h2> Hallo ini halaman contact us </h2>
+
+<div class="title">
+        <h3 class="quicksandBold-font">Follow us on Sosial Media</h3>
+</div>
+<div class="menu">
+                        <ul>
+                            <li>
+                                <span>
+                                    <img src="https://adopsi.org/images/home/icon-instagram.svg" alt="@adopsiapp" width="25">
+                                </span>
+                                <a href="https://www.instagram.com/kittycare_center/" target="_blank">@kittycare_center</a>
+                            </li>
+                            <li>
+                                <span>
+                                    <img src="https://adopsi.org/images/home/icon-fb.svg" alt="Adopsi" width="25">
+                                </span>
+                                <a href="https://www.facebook.com/adopsiapp" target="_blank">KittyCareCenter</a>
+                            </li>
+                        </ul>
+                    </div>
+    <br><br>                
 <?= $this->endSection('content'); ?>
